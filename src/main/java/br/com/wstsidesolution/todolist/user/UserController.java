93b9e121ce51611fbe41c760dd6b832669a4f1d0
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-10-15 18:55:15 -0300

    feat: encrypting password with BCrypt lib

--- src/main/java/br/com/wstsidesolution/todolist/user/UserController.java
+++ src/main/java/br/com/wstsidesolution/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -21,6 +23,10 @@ public class UserController {
     if (user != null) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists.");
     }
+
+    var encryptedPassword = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(encryptedPassword);
 
     var userCreated = this.userRepository.save(userModel);
 
